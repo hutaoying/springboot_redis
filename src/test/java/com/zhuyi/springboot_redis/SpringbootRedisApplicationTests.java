175924cb8618f24a9e31bfa6b5cd4396070618d8
hutao <tyhu317@163.com>
2020-03-27 22:55:43 +0800

    init

---
+++ src/test/java/com/zhuyi/springboot_redis/SpringbootRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhuyi.springboot_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
