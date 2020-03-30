0b6f70fafd05c3ea92833e90ccad77c78f130422
hutaoying <tyhu317@163.com>
2020-03-30 10:41:35 +0800

    checkout lettuce

--- src/main/java/com/zhuyi/springboot_redis/jedis/controller/JedisController.java
+++
@@ -1,8 +0,0 @@
-package com.zhuyi.springboot_redis.jedis.controller;
-
-import org.springframework.data.redis.core.RedisTemplate;
-import redis.clients.jedis.JedisCluster;
-
-public class JedisController {
-
-}
